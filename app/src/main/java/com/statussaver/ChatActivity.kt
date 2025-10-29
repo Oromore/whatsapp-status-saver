aeed6d31521d99adfefe52b656191b47f3dbff2c
Oromore <davidemma005@gmail.com>
2025-10-29 19:28:42 +0100

    Chat Fix 1

--- app/src/main/java/com/statussaver/ChatActivity.kt
+++ app/src/main/java/com/statussaver/ChatActivity.kt
@@ -3,7 +3,7 @@ package com.statussaver
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android:widget.Toast
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.statussaver.databinding.ActivityChatBinding
 

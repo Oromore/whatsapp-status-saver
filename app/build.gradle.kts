dba9302c4c9d3dd0441a7957642a1487e0e970e5
Oromore <davidemma005@gmail.com>
2025-12-10 01:28:44 +0100

    ID Change and Version Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.statussave.app"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "Status-Saver-2026")
